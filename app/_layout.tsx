704f2954d20d693cd560f9370b92f1605510a23b
James Sartelle <james.sartelle@extra.app>
2024-08-24 16:34:54 -0500

    Tamagui working

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,33 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native'
 import { Stack } from 'expo-router'
+import { Platform, useColorScheme } from 'react-native'
+import { TamaguiProvider } from 'tamagui'
+
+import { tamaguiConfig } from '../tamagui.config'
+
+if (Platform.OS === 'web') {
+  /* FIXME fix FOUC */
+  import('../tamagui-web.css')
+}
 
 export default function RootLayout() {
-  /* TODO support dark mode - try Tamagui */
+  const colorScheme = useColorScheme()
+
   return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{ title: 'Todos' }}
-      />
-      <Stack.Screen
-        name="addItem"
-        options={{ presentation: 'modal', title: 'New Item' }}
-      />
-    </Stack>
+    <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme!}>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="index" options={{ title: 'Todos' }} />
+          <Stack.Screen
+            name="addItem"
+            options={{ presentation: 'modal', title: 'New Item' }}
+          />
+        </Stack>
+      </ThemeProvider>
+    </TamaguiProvider>
   )
 }
